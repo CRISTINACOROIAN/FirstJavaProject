10adf196a977d1d6f993175cf54d7f3b376548c8
CRISTINACOROIAN <93049583+CRISTINACOROIAN@users.noreply.github.com>
2021-10-25 20:01:05 +0300

    test

---
+++ src/Lab2.java
@@ -0,0 +1,13 @@
+public class Lab2 {
+    public static void main(String[] args){
+
+
+
+    }
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,35 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
+        System.out.println("Hello");
+        System.out.println("Cristina");
+
+        int myInt = 15;
+        int mySecondInt = myInt + 16;
+        System.out.println(mySecondInt);
+
+        int myNumber = 160;
+        int mySecondNumber = myNumber / 4;
+        System.out.println(mySecondNumber);
+
+        long myLong = - 5;
+        long mySecondLong = 8;
+        long myThirdLong = myLong + mySecondLong * 6;
+        System.out.println(myThirdLong);
+
+         int m1,m2,m3,m4,tot;
+         m1 = 20;
+         m2 = -3;
+         m3 = 5;
+         m4 = 8;
+         tot = m1 + m2 * m3 / m4;
+         System.out.println(tot);
+
+
+
+
+
+
     }
 
 
