c5cfca841e3988f80455b7b4ebbc5883b1c14f99
CRISTINACOROIAN <93049583+CRISTINACOROIAN@users.noreply.github.com>
2021-11-01 09:12:21 +0200

    Lab2.java

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,13 +1,33 @@
 public class Lab2 {
-    public static void main(String[] args){
+    public static int x = 3;
+    public static int z = 3;
 
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        Print print = new Print();
+
+        double result = calc.sum(x, z);
+        System.out.println(result);
+
+        double substractResult = calc.substract(x, z);
+        System.out.println(substractResult);
+
+        double multiplyResult = calc.multiply(x, z);
+        System.out.println(substractResult);
+
+        print.printStars();
+        print.printJava();
+        print.printFata();
+
+    }
 
 
     }
 
 
 
-
-
-
 }
+
+
+
